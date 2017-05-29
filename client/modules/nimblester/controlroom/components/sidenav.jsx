e1b7137aa292e750d0da04c3fdfccc23fd769fe2
Sander de Jonge <sander@web-weave.com>
2017-05-29 11:32:45 +0200

    removed stuff

--- client/modules/nimblester/controlroom/components/sidenav.jsx
+++ client/modules/nimblester/controlroom/components/sidenav.jsx
@@ -28,9 +28,7 @@ class SideNav extends Component {
                         className="material-icons">menu</i></a></li>
                 </ul>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
+
                 </ul>
             </div>
         </nav>
