b03a8094ba18c1754fd2ccc928b56e9a03a45cc9
Sander de Jonge <sander@web-weave.com>
2017-05-29 11:39:42 +0200

    changed one in a dropdown

--- client/modules/nimblester/controlroom/components/sidenav.jsx
+++ client/modules/nimblester/controlroom/components/sidenav.jsx
@@ -9,29 +9,39 @@ class SideNav extends Component {
 
     componentDidMount() {
         $('.button-sidenav').sideNav();
+        $(".dropdown-button").dropdown();
     }
 
     render() {
         return (
-        <nav>
-            <ul id="slide-out" className="side-nav">
-                <li className="sidenav-person"><a href="#"><i className="material-icons">perm_identity</i> Welcome {Meteor.user().profile.name}</a></li>
-                <li><div className="divider"></div></li>
-                <li><a href="/controlroom"><i className="material-icons">dashboard</i> Controlroom</a></li>
-                <li><a href="/controlroom/sales"><i className="material-icons">loyalty</i> Sales</a></li>
-                <li><a href="/controlroom/products"><i className="material-icons">redeem</i> Products</a></li>
-                <li><a href="/controlroom/customers"><i className="material-icons">supervisor_account</i> Customers</a></li>
-            </ul>
-            <div className="nav-wrapper">
-                <ul className="left">
-                    <li> <a href="#" data-activates="slide-out" className="button-sidenav"><i
-                        className="material-icons">menu</i></a></li>
+            <div>
+                <ul id="products_dropdown" className="dropdown-content">
+                    <li><a href="/controlroom/products">All products</a></li>
                 </ul>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <nav>
+                    <ul id="slide-out" className="side-nav">
+                        <li className="sidenav-person"><a href="#"><i className="material-icons">perm_identity</i>
+                            Welcome {Meteor.user().profile.name}</a></li>
+                        <li>
+                            <div className="divider"></div>
+                        </li>
+                        <li><a href="/controlroom"><i className="material-icons">dashboard</i> Controlroom</a></li>
+                        <li><a href="/controlroom/sales"><i className="material-icons">loyalty</i> Sales</a></li>
+                        <li><a className="dropdown-button" href="#!" data-activates="products_dropdown"><i className="material-icons">redeem</i> Products</a></li>
+                        <li><a href="/controlroom/customers"><i className="material-icons">supervisor_account</i>
+                            Customers</a></li>
+                    </ul>
+                    <div className="nav-wrapper">
+                        <ul className="left">
+                            <li><a href="#" data-activates="slide-out" className="button-sidenav"><i
+                                className="material-icons">menu</i></a></li>
+                        </ul>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
 
-                </ul>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </nav>
         );
     }
 
