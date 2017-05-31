41b42f854f65e3603384e9acdfe6da1659733382
Sander de Jonge <sander@web-weave.com>
2017-05-31 10:59:08 +0200

    state of the editor finally working...

--- client/modules/nimblester/controlroom/products/components/new_product.jsx
+++ client/modules/nimblester/controlroom/products/components/new_product.jsx
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
 import {Editor} from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import {stateToHTML} from 'draft-js-export-html';
 import $ from 'jquery';
 
 class NewProduct extends Component {
     constructor(props) {
         super(props);
-        this.state = {productImage: "https://placehold.it/350x350"};
+        this.state = {productImage: "https://placehold.it/350x350", editorState: ''};
     }
 
     componentDidMount() {
-        $(".control-product-image-holder").mouseenter(function(){
+        $(".control-product-image-holder").mouseenter(function () {
             $(".overlay").fadeIn();
         });
-        $(".control-product-image-holder").mouseleave(function(){
+        $(".control-product-image-holder").mouseleave(function () {
             $(".overlay").fadeOut();
         });
 
@@ -26,7 +27,17 @@ class NewProduct extends Component {
 
         return (
             <div>
-                <h4>Add a new product</h4>
+                <div className="row">
+                    <div className="col s9">
+                        <h4>Add a new product</h4>
+                    </div>
+                    <div className="col s3">
+                        <button onClick={this.saveProduct.bind(this)} className="btn waves-effect waves-light save-product-button" name="action">
+                            Save
+                            <i className="material-icons right">send</i>
+                        </button>
+                    </div>
+                </div>
                 <div className="divider"></div>
                 <div className="row">
                     <div className="col s5">
@@ -35,14 +46,15 @@ class NewProduct extends Component {
                             <div className='overlay'><p><i className="medium material-icons">edit</i></p></div>
                             <img className="control-product-image" src={this.state.productImage}/>
                         </div>
-                        <input onChange={this.handleUpload.bind(this)} type="file" className="control-product-img-input" />
+                        <input id="product-image" onChange={this.handleUpload.bind(this)} type="file"
+                               className="control-product-img-input"/>
                     </div>
                     <div className="col s7">
                         <div className="row">
                             <div className="col s12">
                                 <h5>Product title</h5>
                                 <div className="input-field">
-                                    <input id="product-title" type="text" className="validate"/>
+                                    <input ref="product-title" id="product-title" type="text" className="validate"/>
                                 </div>
                             </div>
                         </div>
@@ -50,6 +62,7 @@ class NewProduct extends Component {
                             <div className="col s12">
                                 <h5>Product description</h5>
                                 <Editor
+                                    onEditorStateChange={ this.handleEditorChange.bind(this) }
                                     toolbarClassName="home-toolbar"
                                     wrapperClassName="home-wrapper"
                                     editorClassName="home-editor"
@@ -63,14 +76,35 @@ class NewProduct extends Component {
     }
 
     handleUpload(image) {
-        this.generatePreview(image);
-        console.log(URL.createObjectURL(image.target.files[0]));
+        this.getBase64();
+
     }
 
-    generatePreview(image) {
-        //Some l33t hax to generate a preview
-        this.setState({productImage: URL.createObjectURL(image.target.files[0])});
+    getBase64() {
+        const filesSelected = document.getElementById("product-image").files;
+
+        if (filesSelected.length > 0) {
+            const fileToLoad = filesSelected[0];
+
+            const fileReader = new FileReader();
+
+            fileReader.addEventListener('load', (fileLoadedEvent) => {
+                this.setState({productImage: fileLoadedEvent.target.result});
+            });
+
+
+            fileReader.readAsDataURL(fileToLoad);
+        }
     }
+
+    handleEditorChange(contentState) {
+        this.setState({editorState: stateToHTML(contentState.getCurrentContent())});
+    }
+
+    saveProduct(e) {
+        console.log(e);
+    }
+
 
 }
 
